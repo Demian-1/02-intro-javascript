1836ccda65701eb1b5d1300bb07282915a611d84
Demian-1 <sergio_demian_ae@hotmail.com>
2022-12-25 10:28:41 -0600

    Vimos promesas, then y catch

--- src/bases/08-imports-exports-lambdas-findfilter.js
+++ src/bases/08-imports-exports-lambdas-findfilter.js
@@ -1,14 +1,14 @@
 // Se hace como una desestructuracion:       los que son como default no llevan {}
-import  heroes, {owners} from "./data/heroes"
+import  heroes from "../data/heroes"
 
-console.log(owners);
+// console.log(owners);
 
-const getHeroeByID = (id) => {
+export const getHeroeByID = (id) => {
     return heroes.find( (heroe) => heroe.id === id);
 }
 
-console.log(getHeroeByID(4));
+// console.log(getHeroeByID(4));
 
-const getHeroesByOwner = ( owner ) =>  heroes.filter((heroe)=>heroe.owner===owner);
+export const getHeroesByOwner = ( owner ) =>  heroes.filter((heroe)=>heroe.owner===owner);
 
-console.log(getHeroesByOwner('Marvel'));
+// console.log(getHeroesByOwner('Marvel'));
